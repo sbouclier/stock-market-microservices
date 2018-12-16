4aaf7a2ca5e5f27eaf0e8263e219dbd6758e9c33
Stéphane Bouclier <stephane.bouclier@quicksign.com>
2018-12-16 19:37:40 +0100

    remove comments code

--- stock-prices-consumer/src/main/java/com/github/sbouclier/stockmarketmicroservices/StockPricesConsumerApplication.java
+++ stock-prices-consumer/src/main/java/com/github/sbouclier/stockmarketmicroservices/StockPricesConsumerApplication.java
@@ -2,28 +2,14 @@ package com.github.sbouclier.stockmarketmicroservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jms.support.converter.MessageType;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-//@EnableJms
 public class StockPricesConsumerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StockPricesConsumerApplication.class);
     }
-
-/*    @Bean // Serialize message content to json using TextMessage
-    public MessageConverter jacksonJmsMessageConverter() {
-        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-        converter.setTargetType(MessageType.TEXT);
-        converter.setTypeIdPropertyName("_type");
-        return converter;
-    }*/
 
 }
