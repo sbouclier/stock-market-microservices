1fed3a5191ce34961578695881fda03bfeed0d5d
Stéphane Bouclier <stephane.bouclier@quicksign.com>
2018-12-31 16:30:00 +0100

    config discovery client

--- stock-prices-consumer/src/main/java/com/github/sbouclier/stockmarketmicroservices/StockPricesConsumerApplication.java
+++ stock-prices-consumer/src/main/java/com/github/sbouclier/stockmarketmicroservices/StockPricesConsumerApplication.java
@@ -2,8 +2,10 @@ package com.github.sbouclier.stockmarketmicroservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class StockPricesConsumerApplication {
 
     public static void main(String[] args) {
